37fcd9d3113a74906a86431b74083cc674efb7ad
payneLee <peinlcy@sina.cn>
2016-03-08 19:58:28 +0800

    4sum

--- FourSum.cpp
+++ FourSum.cpp
@@ -9,6 +9,41 @@ public:
     vector<vector<int> > fourSum(vector<int>& nums, int target) {
     	int length = nums.size();
         vector<vector<int> > result;
+        if (length < 4) {
+            return result;
+        }
+        sort(nums.begin(), nums.end());
+        unordered_map<int, vector<pair<int, int> > > sumMap;
+        for (int i = 0; i < length; i++) {
+            for (j = i+1; j < length; j++) {
+                int tmpTarget = nums[i] + nums[j];
+                if (sumMap.find(tmpTarget) == sumMap.end()) {
+                    pair<int, int> p = make_pair(nums[i], nums[j]);
+                    vector<pair<int, int> > pairVec;
+                    pairVec.push_back(p);
+                    sumMap[tmpTarget] = pairVec;
+                } else {
+                    vector<pair<int, int> > pairVec = sumMap[tmpTarget];
+                    pair<int, int> p = make_pair(i, j);
+                    pairVec.push_back(p);
+                    sumMap[tmpTarget] = pairVec;
+                }
+            }
+        }
+        for (auto iter = sumMap.begin(); iter != sumMap.end(); iter++) {
+            int tmpTarget = iter->first;
+            vector<pair<int, int> > pairVec1 = iter->second;
+            if ((target - tmpTarget == tmpTarget) && pairVec1.size() == 1) {
+                continue;
+            }
+            if (sumMap.find(target - tmpTarget) != sumMap.end()) {
+                vector<pair<int, int> > pairVec2 = sumMap[target - tmpTarget];
+                for (pair<int, int> p = pairVec2)
+            }
+        }
+
+
+
         for (int i = 0; i < length-3; i++) {
         	for (int j = i+1; j < length-2; j++) {
         		int tmpTarget = target - nums[i] - nums[j];
@@ -37,7 +72,7 @@ int main() {
 	Solution a;
 	int arr[] = {1,0,-1,0,-2,2};
 	vector<int> vec(arr,arr+(sizeof(arr)/sizeof(int)));
-	vector<vector<int> > res = a.FourSum(vec, 0);
+	vector<vector<int> > res = a.fourSum(vec, 0);
 	for (int i = 0; i < res.size(); i++) {
 		vector<int> tmpVec = res[i];
         for (int j = 0; j < tmpVec.size(); j++) {
@@ -45,5 +80,6 @@ int main() {
         }
         cout<<endl;
 	}  
+    pair<int, int> p1= make_pair(1, 1);
 	return 0;
 }
