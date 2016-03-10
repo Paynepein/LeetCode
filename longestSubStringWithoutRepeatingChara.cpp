c63a383f020f4ef140b3a72894be14eda716b34e
payneLee <peinlcy@sina.cn>
2016-03-10 18:27:07 +0800

    longest substring without repeating character AC

--- longestSubStringWithoutRepeatingChara.cpp
+++ longestSubStringWithoutRepeatingChara.cpp
@@ -6,25 +6,63 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLength = 0, tmpLength = 0, length = s.size();
+        int maxLength = 0, tmpLength = 1, length = s.size();
         unordered_map<char, int> map;
-        int start = 0;
-        for (int i = 0; i < length; i++) {
-        	for (int j = i+1; i < length; ++j) {
-        		
-        	}
-        	if (map.find(s[i]) == map.end()) {
-        		map[s[i]] = i;
-        		++start;
-        		++tmpLength;
-        		if (tmpLength > maxLength) {
-        			maxLength = tmpLength;
-        		}
-        	}
+        int start = 0, i = 0, j = 0;
+        for (; i < length - maxLength; ++i) {
+            j = j + 1;
+            tmpLength = j - i;
+            map[s[i]] = i;
+            for (; j < length; ++j) {
+                // if (j == 4) cout<<"b:"<<map['b']<<endl;
+                if (map.find(s[j]) != map.end() && map[s[j]] >= i && map[s[j]] < j) {
+                    if (tmpLength > maxLength) {
+                        start = i;
+                        maxLength = tmpLength;
+                    }
+                    i = map[s[j]];
+                    map[s[j]] = j;
+                    break;
+                } else {
+                    ++tmpLength;
+                    map[s[j]] = j;
+                }
+            }
+            if (j == length && maxLength < tmpLength) {
+                maxLength = tmpLength;
+                start = i;
+            }
         }
+        cout<<start<<" "<<maxLength<<endl;
+        return maxLength;
     }
 };
 
 int main() {
+    Solution a;
+    string s1("anviaj");
+    string s2("b");
+    string s3("bb");
+    string s4("bbb");
+    string s5("abcabcbb");
+    string s6("abb");
+    string s7("dcdc");
+    string s8("dvdc");
+    string s9("bab");
+    string s10("baa");
+    string s11("bba");
+    cout<<s5<<endl;
+    a.lengthOfLongestSubstring(s1);
+    a.lengthOfLongestSubstring(s2);
+    a.lengthOfLongestSubstring(s3);
+    a.lengthOfLongestSubstring(s4);
+    a.lengthOfLongestSubstring(s5);
+    a.lengthOfLongestSubstring(s6);
+    a.lengthOfLongestSubstring(s7);
+    a.lengthOfLongestSubstring(s8);
+    a.lengthOfLongestSubstring(s9);
+    a.lengthOfLongestSubstring(s10);
+    a.lengthOfLongestSubstring(s11);
+            // cout<<maxLength<<" "<<start<<endl;
 	return 0;
 }
