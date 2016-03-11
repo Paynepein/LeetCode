cafc3fcebfa78614db4fedf425cc3abe51497ed1
Paynepein <peinlcy@sina.cn>
2016-03-11 08:21:44 +0800

    happy number new

--- HappyNumber.cpp
+++ HappyNumber.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
-#include <unordered_map>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
-    bool isHappy(int n) {
-  		int sum = 0;
-  		unordered_map<int, int> map;
-  		if (n < 10) {
-  			return (n == 1 || n == 7) ? true : false;
-  		}
-        while (true) {
-        	sum = 0;
-        	while (n > 9) {
-        		sum += sqrt(n%10);
-        		n /= 10;
-        	}
-        	sum += sqrt(n);
-        	if (sum < 10) {
-        		return (n == 1 || n == 7) ? true : false;
-        	}
-        }
-    }
+	bool isHappyNumber(int n) {
+		int sum = n;
+		while (sum > 9) {
+			n = sum;
+			sum = 0;
+			while (n > 9) {
+				sum += pow(n % 10, 2);
+				n /= 10;
+			}
+			sum += pow(n, 2);
+			if (sum < 10) {
+				break;
+			}
+		}
+		return (sum == 1 || sum == 7) ? true : false;
+	}
 };
 
 int main() {
 	Solution a;
-	for (int i = 1; i < 20; i++)
-	    cout<<a.isHappy(i)<<endl;
+	for (int i = 0; i < 20; ++i) {
+		cout<<a.isHappyNumber(i)<<endl;
+	}
 	return 0;
 }
