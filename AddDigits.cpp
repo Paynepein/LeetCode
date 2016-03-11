366cbb18aa2fa5a3ee74d3fb28361ad35fac44c4
payneLee <peinlcy@sina.cn>
2016-03-11 11:11:07 +0800

    add digits ac

---
+++ AddDigits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) return 0;
+        return (num % 9 == 0) ? 9 : num % 9;
+    }
+};
+
+int main() {
+	return 0;
+}
