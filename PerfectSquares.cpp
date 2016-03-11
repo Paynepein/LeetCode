041214351f9d37344a1043bfbaf4853bc496b573
payneLee <peinlcy@sina.cn>
2016-03-11 20:25:56 +0800

    perfect squares to be done

--- PerfectSquares.cpp
+++ PerfectSquares.cpp
@@ -8,17 +8,14 @@ public:
     	int *nums = new int[n+1];
     	nums[1] = 1;
     	for (int i = 2; i <= n; ++i) {
-    		if (i == pow((int)sqrt(i), 2)) {
-    			nums[i] = 1;
-    		} else {
-    			nums[i] = i;
-    			int start = 1, end = i-1;
-    			while (start <= end) {
-    				if (nums[start] + nums[end] < nums[i]) {
-    					nums[i] = nums[start] + nums[end];
-    				}
-    				++start;
-    				--end;
+    		nums[i] = i;
+    	}
+    	for (int i = 2; i <= n; ++i)
+    	{
+    		for (int j = 0; j <= i; ++j) {
+    			if (i == j*j) {
+    				nums[i] = 1;
+    				break;
     			}
     		}
     	}
