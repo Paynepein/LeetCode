7d28ac7950ff4547908276337834985b0beca628
payneLee <peinlcy@sina.cn>
2016-03-26 11:21:14 +0800

    longest palindrome substring ac

--- longestPalindromicSubstring.cpp
+++ longestPalindromicSubstring.cpp
@@ -15,6 +15,8 @@ public:
 	}
 
     string longestPalindrome(string s) {
+    	s = preProcess(s);
+    	cout<<s<<endl;
         int length = s.size(), maxLength = 0;
 		int *palinArr = new int[length];
 		palinArr[0] = 0;
@@ -23,15 +25,14 @@ public:
 		string res;
 		for (int i = 1; i < length; ++i) {
 			mirror = 2*center - i;
-			mirror = mirror < 0 ? 0 : mirror;
 			left = center - palinArr[center];
 			right = center + palinArr[center];
-			if (palinArr[mirror]+i < right) {
+			if (mirror >= 0 && palinArr[mirror]+i < right) {
 				palinArr[i] = palinArr[mirror];
 			} else {
 				center = i;
-				left = 2*i - right - 1;
 				right += 1;
+				left = 2*i - right;
 				while (left>=0 && right < length && s[left] == s[right]) {
 					--left;
 					++right;
@@ -41,7 +42,7 @@ public:
 					maxLength = palinArr[i];
 					res = "";
 					for (int j = 0; j < maxLength; ++j) {
-						res += s[left+j*2+1];
+						res += s[left+j*2+2];
 					}
 				}
 			}
@@ -53,18 +54,20 @@ public:
 int main() {
 	int num;
 	// cin>>num;
-	num = 3;
+	// num = 3;
 	string str;
 	str = "abababa";
 	str = "aaaabaa";
-	// str = "acacdas";
+	str = "acacdas";
 	// str = "ababa";
-	str = preProcess(str);
-	cout<<maxPalindromicSubstring(str)<<endl;
+	// str = preProcess(str);
+	// cout<<maxPalindromicSubstring(str)<<endl;
 	// while (num--) {
 	// 	cin>>str;
 	// 	str = preProcess(str);
 	// cout<<maxPalindromicSubstring(str)<<endl;
 	// }
+	Solution a;
+	cout<<a.longestPalindrome(str)<<endl;
 	return 0;
 }
