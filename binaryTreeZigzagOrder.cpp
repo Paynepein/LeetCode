30d8001bfe9ac5c2f0c53117efb26676ce9d865b
Paynepein <peinlcy@sina.cn>
2016-04-07 23:56:16 +0800

    binary tree zigzag level order to be done

---
+++ binaryTreeZigzagOrder.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+ /**
+  * 用栈遍历，先进后出
+  */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        
+    }
+};
+
+int main() {
+	return 0;
+}
