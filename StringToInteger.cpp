3d5626b789ee34d2a84e72657d6da104311bcc84
payneLee <peinlcy@sina.cn>
2016-04-09 12:21:49 +0800

    string to integer ac

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -7,19 +7,26 @@ class Solution {
 public:
 	int myAtoi(string str) {
 		int start = str.find_first_not_of(" \t");
-		if (start == string::npos || !isdigit(str[start]) || str[start] != '-' || str[start] != '+') return 0;
+		if (start == string::npos || (!isdigit(str[start]) && str[start] != '-' && str[start] != '+')) return 0;
 		int sign = str[start] == '-' ? -1 : 1;
 		if (!isdigit(str[start])) ++start;
 		int length = str.size();
 		int result = 0, tmp = 0;
-		for (; start < length; ++start) {
-			tmp = str[start] - '0';
-			if (result > (numeric_limits<int>::max() / 10) || result > (numeric_limits<int>::max() - tmp)) {
-				
+		for (; start < length;) {
+			if (!isdigit(str[start])) break;
+			tmp = str[start++] - '0';
+			if (result > (numeric_limits<int>::max() / 10) || result*10 > (numeric_limits<int>::max() - tmp)) {
+				return sign == 1 ? numeric_limits<int>::max() : numeric_limits<int>::min();
 			}
+			result *= 10;
+			result += tmp;
 		}
+		return result*sign;
 	}
 };
 int main() {
+	Solution a;
+	// cout<<numeric_limits<int>::max()<<endl<<numeric_limits<int>::min()<<endl;
+	cout<<a.myAtoi(" -+1-2")<<endl;
 	return 0;
 }
