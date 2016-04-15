0a4d0d310dea0523d2952d4170e759c3a07dfaba
Paynepein <peinlcy@sina.cn>
2016-04-16 00:33:30 +0800

    palindrome number to be done

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		if (x < 0) return false;
+		if (x <= 9) return true;
+		if (x % 10 == 0) return false;
+		int tmp = 0;
+		while (tmp <= x) {
+			tmp *= 10;
+			tmp += x % 10;
+			x /= 10;
+		}
+		return (tmp == x) || (tmp/10 == x);
+	}
+};
+int main() {
+	Solution a;
+	int x = 11;
+	cout<<a.isPalindrome(x)<<endl;
+	return 0;
+}
