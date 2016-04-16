2d828a4dc546955693ac97947878d4164202a967
Paynepein <peinlcy@sina.cn>
2016-04-16 23:54:42 +0800

    palindrome number ac

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -13,38 +13,48 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
     	if (head == NULL || head->next == NULL) return true;
-        ListNode *p1 = head, *p2 = head, *p3 = head;
-        while (p1->next) {
-        	p3 = p2;
+        ListNode *p1 = head, *p2 = head, *p3;
+        while (p1 && p1->next) {
 			p2 = p2->next;
         	p1 = p1->next->next;
         }
-        if (p1 == NULL) {
-        	p1 = p2->next;
-        } else {
-        	p3 = p2;
-        	p2 = p2->next;
+        p2 = p1 == NULL ? p2 : p2->next;
+        p1 = NULL, p3 = p2->next;
+        while (p3) {
+        	p2->next = p1;
+        	p1 = p2;
+        	p2 = p3;
+        	p3 = p3->next; 
         }
-        p1 = reverse(p2);
-        p2->next = p3;
+        p2->next = p1;
 
-        p2 = head, p3 = p1;
-        
+        p1 = head;
+        while (p2 != NULL) {
+        	if (p1->val != p2->val) return false;
+        	p2 = p2->next;
+        	p1 = p1->next;
+        }
+        return true;
     }
-private:
-	ListNode* reverse(ListNode *head) {
-		if (head == NULL || head->next == NULL) return head;
-		ListNode *p1 = head, *p2 = head->next, *p3 = head;
-		while (p2) {
-			p3 = p2->next;
-			p2->next = p1;
-			p1 = p2;
-			p2 = p3;
-		}
-		return p1;
-	}
 };
 
 int main() {
+	Solution a;
+	ListNode head(1);
+	ListNode p1(2);
+	ListNode p2(3);
+	ListNode p3(4);
+	ListNode p4(4);
+	ListNode p5(2);
+	ListNode p6(1);
+
+	head.next = &p1;
+	p1.next = &p2;
+	p2.next = &p3;
+	p3.next = &p4;
+	p4.next = &p5;
+	p5.next = &p6;
+
+	cout<<a.isPalindrome(&head)<<endl;
 	return 0;
 }
