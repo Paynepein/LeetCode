bce72b7c0c3bf5fb05ed586b08d6623b9798df2c
payneLee <peinlcy@sina.cn>
2016-04-22 14:26:36 +0800

    wildcard matching DP ac

--- WildcardMatching.cpp
+++ WildcardMatching.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+// dp[i][j]==true 表示s[0:i-1]与p[0:j-1]匹配
+
+// if p[j-1] != '*' && p[j-1] != '?'
+//     dp[i][j] = true if dp[i-1][j-1] && s[i-1] == p[j-1]
+
+// if p[j-1] == '?'
+//     dp[i][j] = true if dp[i-1][j-1] == true
+
+// if p[j-1] == '*'
+//     dp[i][j] = true if 
+//                         1.匹配0个 dp[i][j-1]
+//                         2.匹配1个 dp[i-1][j-1]
+//                         3.匹配多个 dp[i-1][j]
 
 class Solution {
 public:
     bool isMatch(string s, string p) {
         int lenS = s.size(), lenP = p.size();
-        if (lenP == 0) return lenS == 0;
-        int indexP = p.find_first_not_of("*");
-        int rIndexP = p.find_last_not_of("*");
-        if (indexP == -1) return true;
-        if (p[0] == '*') {
-        	if (p[indexP] == '?') {
-        		
-        	}
+        vector<vector<bool> >dp(lenS+1, vector<bool>(lenP+1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= lenS; ++i) {
+            for (int j = 1; j <= lenP; ++j) {
+                if (i > 0 && p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (i > 0 && p[j-1] != '*' && s[i-1] == p[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-1];
+                    if (i > 0 && dp[i][j] == false) {
+                        dp[i][j] = dp[i-1][j-1] || dp[i-1][j];
+                    } 
+                }
+            }
         }
-    }
-
-    string prepare(string s) {
-    	string res;
-    	int len = s.size();
-    	for (int i = 0; i < len; ++i) {
-    		if (s[i] != '*') {
-    			res += s[i];
-    		} else if (i == 0 || s[i-1] != '*') {
-    			res += s[i];
-    		}
-    	}
-    	cout<<res<<endl;
-    	return res;
+        return dp[lenS][lenP];
     }
 };
-
-// babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb
-// "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a"
-// "***bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a"
-// "?**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a"
-
 
 int main() {
 	Solution a;
