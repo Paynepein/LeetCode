7746ea1a3b173825d86a6323c33140aae1b1bcf1
Paynepein <peinlcy@sina.cn>
2016-04-25 23:17:25 +0800

    best time to buy sell stockI ac

--- BestTimeBuySellStock.cpp
+++ BestTimeBuySellStock.cpp
@@ -7,10 +7,10 @@ public:
     int maxProfit(vector<int>& prices) {
         int length = prices.size();
         if (length < 2) return 0;
-        int sell = prices[0], profit = 0;
+        int buy = prices[0], profit = 0;
         for (int i = 1; i < length; ++i) {
-        	sell = min(sell, prices[i]);
-        	profit = max(profit, prices[i] - sell);
+        	buy = min(buy, prices[i]);
+        	profit = max(profit, prices[i] - buy);
         }
         return profit;
     }
