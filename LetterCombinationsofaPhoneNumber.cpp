cd52267c97c3e0aa192967304f1027b1c432b330
payneLee <peinlcy@sina.cn>
2016-04-29 15:29:21 +0800

    letter combinations of phone number ac

---
+++ LetterCombinationsofaPhoneNumber.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char, string> map;
+        map['2'] = "abc";
+        map['3'] = "def";
+        map['4'] = "ghi";
+        map['5'] = "jkl";
+        map['6'] = "mno";
+        map['7'] = "pqrs";
+        map['8'] = "tuv";
+        map['9'] = "wxyz";
+        return combinations(map, digits, 0, digits.size());
+    }
+
+	vector<string> combinations(unordered_map<char, string>& map, string& digits, int index, int length) {
+		vector<string> res;
+		if (index == length) return res;
+		string str = map[digits[index]];
+		int len = str.size();
+		for (int i = 0; i < len; ++i) {
+			string s = "";
+			s += str[i];
+			vector<string> vec = combinations(map, digits, index+1, length);
+			int l = vec.size();
+			if (l == 0) {
+				res.push_back(s);
+			} else {
+				for (int j = 0; j < l; ++j) {
+        			res.push_back(s + vec[j]);
+        		}	
+			}
+        	// s = "";
+		}
+		return res;
+	}
+};
+
+int main() {
+	Solution a;
+	vector<string> vec = a.letterCombinations("2");
+	for (int i = 0; i < vec.size(); ++i) {
+		cout<<vec[i]<<endl;
+	}
+	return 0;
+}
--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    	int length = strs.size();
+    	if (length == 0) return "";
+    	if (length == 1) return strs[0];
+		vector<int> pre(length, 0);
+		string str1 = strs[0];
+		int len = str1.size(), i = 0;
+		for (; i < len; ++i) {
+			for (int j = 1; j < length; ++j) {
+				string s = strs[j];
+				if ( i == s.size() || s[i] != str1[i]) {
+					return str1.substr(0, i);
+				}
+			}
+		}
+		return i == len ? str1 : "";
+    }
+};
+
 int main() {
+	Solution a;
+	string strs[] = {"abc", "abca", "abca"};
+	vector<string> vec(strs, strs + 3);
+	cout<<a.longestCommonPrefix(vec)<<endl;
 	return 0;
 }
