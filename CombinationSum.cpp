5db66631f141e68b2501c64f53ca53227f8768c6
Paynepein <peinlcy@sina.cn>
2016-05-01 01:10:03 +0800

    combination sum ac

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -7,20 +8,26 @@ public:
     vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int> > res;
-
+        vector<int> vec;
+        // cout<<candidates[0]<<candidates[1]<<candidates[2]<<candidates[3]<<endl;
+        combination(candidates, 0, candidates.size(), res, vec, target);
         return res;
     }
 
-    pair<bool, vector<int> > combination(vector<int>& candidates, int index, vector<int> vec, int target) {
-    	if (target < candidates[index]) {
-    		return make_pair<false, vec>;
-    	} else if (target == candidates[index]) {
-    		vec.push_back(candidates[index]);
-    		return make_pair(true, vec);
-    	} else {
-    		
-    	}
-
+    void combination(vector<int>& candidates, int index, int length, vector<vector<int> >& res, vector<int> vec, int target) {
+        for (int i = index; i < length; ++i) {
+            if (candidates[i] == target) {
+                vec.push_back(candidates[i]);
+                res.push_back(vec);
+            } else if (candidates[i] < target) {
+                vector<int> tmp(vec);
+                tmp.push_back(candidates[i]);
+                combination(candidates, i, length, res, tmp, target - candidates[i]);
+            } else {
+                break;
+            }
+        }
+        return;
     }
 };
 
@@ -29,5 +36,9 @@ int main() {
 	vector<int> vec(arr, arr+4);
 	Solution a;
 	vector<vector<int> > re = a.combinationSum(vec, 7);
+    vector<int> it = re[1];
+    for (int i = 0; i < it.size(); ++i) {
+        cout<<it[i]<<endl;
+    }
 	return 0;
 }
