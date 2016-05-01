a9321bd7985767e466c820193ba92b74146c9f19
Paynepein <peinlcy@sina.cn>
2016-05-01 23:12:55 +0800

    valid parentheses ac

---
+++ ValidParentheses.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+		stack<char> myStack;
+		int length = s.size();
+		if (length == 0) return true;
+		if (length % 2 == 1) return false;
+		unordered_map<char, int> map;
+		map['('] = 1;
+		map['['] = 2;
+		map['{'] = 3;
+		map[')'] = 9;
+		map[']'] = 8;
+		map['}'] = 7;
+		for (int i = 0; i < length; ++i) {
+			if (map[s[i]] <= 3) {
+				myStack.push(s[i]);
+			} else {
+				if (myStack.empty()) return false;
+				char c = myStack.top();
+				myStack.pop();
+				if (map[c] + map[s[i]] != 10) return false;
+			}
+		}
+		return myStack.empty();
+    }
+};
+
+int main() {
+	Solution a;
+	cout<<a.isValid("([{{}])");
+	return 0;
+}
