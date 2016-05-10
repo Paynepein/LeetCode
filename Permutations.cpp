34c00fc78f513c84f834e2966db6daa5624acb25
payneLee <peinlcy@sina.cn>
2016-05-10 10:32:42 +0800

    premutations ac

--- Permutations.cpp
+++ Permutations.cpp
@@ -12,15 +12,27 @@ public:
 			res.push_back(nums);
 			return res;
 		}
-		vector<int> vec(length, 0);
-		vector<bool> used(length, 0);
-		createPermutation(res, vec, nums, 0, length);
+		vector<int> vec;
+		vector<bool> used(length, false);
+		createPermutation(res, vec, used, nums, 0, length);
+		return res;
     }
 
-    void createPermutation(vector<vector<int> >& res, vector<int> vec, vector<int>& nums, int count, int length) {
-    	for (int i = 0; i < length; ++i) {
-    		vec.
+    void createPermutation(vector<vector<int> >& res, vector<int>& vec, vector<bool>& used, vector<int>& nums, int count, int length) {
+    	if (count == length) {
+    		res.push_back(vec);
+    		return;
     	}
+    	for (int i = 0; i < length; ++i) {
+    		if (!used[i]) {
+    			vec.push_back(nums[i]);
+    			used[i] = true;
+    			createPermutation(res, vec, used, nums, count+1, length);
+    			used[i] = false;
+    			vec.pop_back();
+    		}
+    	}
+    	return;
     }
 };
 
