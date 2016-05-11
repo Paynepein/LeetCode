5431959e73e80e031d750ef59f5e49c59d0b96b4
Paynepein <peinlcy@sina.cn>
2016-05-12 00:15:07 +0800

    search in rotate array ac

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -9,20 +9,20 @@ public:
         int length = s.size();
         if (length == 0 || length == 1) return 0    ;
         vector<int> dp(length, 0);
-        int index;
+        int index, maxi = 0;
         for (int i = length - 2; i >= 0; --i) {
             if (s[i] == '(') {
                 index = i + dp[i+1] + 1;
-                if (s[index] == ')') {
+                if (index < length && s[index] == ')') {
                     dp[i] = dp[i+1] + 2;
-                } else {
-                    dp[i] = dp[i+1];
+                    if (index+1 < length) {
+                        dp[i] += dp[index+1];
+                    }
                 }
-            } else {
-                dp[i] = dp[i+1];
             }
+            maxi = max(maxi, dp[i]);
         }
-        return dp[0];
+        return maxi;
     }
 };
 
---
+++ SearchInRotatedArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    	int len = nums.size();
+    	if (len == 0) return -1;
+    	if (len == 1) return nums[0] == target ? 0 : -1;
+        int l = 0, r = len - 1;
+        while (l <= r) {
+        	int m = (l + r) / 2;
+        	if (nums[m] == target) return m;
+        	if (nums[m] > nums[r]) {
+        		if ((nums[m] > target) && (target >= nums[l])) {
+        			r = m-1;
+        		} else {
+        			l = m + 1;
+        		}
+        	} else {
+        		if ((nums[m] < target) && (target <= nums[r])) {
+        			l = m + 1;
+        		} else {
+        			r = m - 1;
+        		}
+        	}
+        } 
+        return -1;
+    }
+};
