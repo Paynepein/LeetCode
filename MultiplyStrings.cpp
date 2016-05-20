7d1899e2fbd4e2f3baf77c453fc8b27353fe60f6
payneLee <peinlcy@sina.cn>
2016-05-20 11:30:26 +0800

    multiply strings ac

--- MultiplyStrings.cpp
+++ MultiplyStrings.cpp
@@ -7,7 +7,12 @@ public:
     string multiply(string num1, string num2) {
         int len1 = num1.size(), len2 = num2.size();
         if (len1 == 0 || len2 == 0) return "";
+        if (len1 < len2) return multiply(num2, num1);
         string res;
+        for (int i = 0; i < len2; ++i) {
+            res = addStr(res, multiplyStr(num1, num2[i], len2-1-i));
+        }
+        return res;
     }
 
     string multiplyStr(string s, char m, int index) {
@@ -23,21 +28,37 @@ public:
     		char a = tmp % 10 + '0';
     		res = a + res;
     	}
-    	return res + tail;
+        if (c > 0) {
+            char a = c + '0';
+            res = a + res;
+        }
+        res += tail;
+    	return res;
     }
 
     string addStr(string s1, string s2) {
     	string res;
-    	int len1 = s1.size(), len2 = s2.size();
+    	int len1 = s1.size(), len2 = s2.size(), sum = 0, c = 0;
     	if (len1 == 0) return s2;
     	if (len2 == 0) return s1;
-    	int len = min(len1, len2);
-    	for (int i = 0; i < len; ++i) {
-    		char c1 = s1[len1-1-i], c2 = s2[len2-1-i];
+    	int len = max(len1, len2);
+        char c1 = s1[len1-1], c2 = s2[len2-1], cc = '0';
+    	for (int i = 0; i < len; ++i, c1 = i < len1 ? s1[len1-1-i] : '0', c2 = i < len2 ? s2[len2-1-i] : '0') {
+            sum = c1 - '0' + c2 - '0' + c;
+            cc = sum % 10 + '0';
+            c = sum / 10;
+            res = cc + res;
     	}
+        if (c > 0) {
+            cc = c + '0';
+            res = cc + res;
+        }
+        return res;
     }
 };
 
 int main() {
+    Solution a;
+    cout<<a.multiply("100", "00")<<endl;
 	return 0;
 }
