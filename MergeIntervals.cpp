9d1f0b1f70eec4ec05b2c75fd36cbaa4e4ffa747
payneLee <peinlcy@sina.cn>
2016-08-15 17:56:54 +0800

    merge intervals to be done

---
+++ MergeIntervals.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        
+    }
+};
+
+int main() {
+	return 0;
+}
