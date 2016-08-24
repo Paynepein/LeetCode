f8e4f89ea29ea3481c53bdaf10a993a8c1f5546e
payneLee <peinlcy@sina.cn>
2016-08-24 14:38:26 +0800

    dungeon game ac

---
+++ DungeonGame.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+  		int m = dungeon.size(), n = dungeon[0].size();
+  		vector<vector<int> > path(m+1, vector<int>(n+1, INT_MAX));
+  		path[m][n-1] = path[m-1][n] = 1;
+  		for (int i = m-1; i >= 0; --i) {
+  			for (int j = n-1; j >= 0; --j) {
+  				path[i][j] = max(1, min(path[i+1][j], path[i][j+1]) - dungeon[i][j]);
+  			}
+  		}
+  		return path[0][0];
+    }
+};
