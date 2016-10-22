78423c518937a4668ca2ff2868375de470446b67
payneLee <peinlcy@sina.cn>
2016-10-22 21:44:45 +0800

    find peak element ac

---
+++ FindPeakElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size()-1;
+        while (low < high) {
+        	int mid1 = (low + high) >> 1;
+        	int mid2 = mid1 + 1;
+        	if (nums[mid1] < nums[mid2]) {
+        		low = mid2;
+        	} else {
+        		high = mid1;
+        	}
+        }
+        return low;
+    }
+};
