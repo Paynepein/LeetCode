2844be4bd4a4141126ac2689169cc38286c7cf4d
payneLee <peinlcy@sina.cn>
2016-10-24 18:41:36 +0800

    Excel Sheet Column Title ac

--- ExcelSheetColumnTitle.cpp
+++ ExcelSheetColumnTitle.cpp
@@ -3,9 +3,8 @@ public:
     string convertToTitle(int n) {
         string res = "";
         while (n) {
-            int r = n % 26 == 0 ? 26 : n % 26;
-        	res += 'A' - 1 + r;
-        	n = (n - r) / 26;
+        	res += 'A' + --n % 26;
+        	n /= 26;
         }
         reverse(res.begin(), res.end());
         return res;
