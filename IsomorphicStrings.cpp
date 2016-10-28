5d4b783f3c9f44f95c7efabca0d7cc35b6a537a9
payneLee <peinlcy@sina.cn>
2016-10-28 16:22:34 +0800

    Isomorphic strings ac

--- IsomorphicStrings.cpp
+++ IsomorphicStrings.cpp
@@ -3,17 +3,12 @@ public:
     bool isIsomorphic(string s, string t) {
     	if (s.size() != t.size()) return false;
     	if (s == t) return true;
-        unordered_map<char, char> smap;
-        unordered_map<char, char> tmap;
+        unordered_map<char, int> smap;
+        unordered_map<char, int> tmap;
         for (int i = 0; i < s.size(); ++i) {
-        	if (smap.find(s[i]) == smap.end() && tmap.find(t[i]) == tmap.end()) {
-        		smap[s[i]] = t[i];
-        		tmap[t[i]] = s[i];
-        	} else if (smap.find(s[i]) != smap.end() && tmap.find(t[i]) != tmap.end()) {
-        		if (smap[s[i]] != t[i] && tmap[t[i]] != s[i]) return false;
-        	} else {
-        		return false;
-        	}
+        	if (smap[s[i]] != tmap[t[i]]) return false;
+        	smap[s[i]] = i+1;
+        	tmap[t[i]] = i+1;
         }
         return true;
     }
