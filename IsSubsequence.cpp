653696fe3d610577b5563d9faa1e48f805415758
payneLee <peinlcy@sina.cn>
2016-12-20 09:48:02 +0800

    is subsequence ac

--- IsSubsequence.cpp
+++ IsSubsequence.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        
+        int start = 0;
+        for (char c : s) {
+        	int pos = t.find_first_of(c, start);
+        	if (pos == string::npos) return false;
+        	start = pos+1;
+        }
+        return true;
     }
 };
