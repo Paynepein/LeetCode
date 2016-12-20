59f5915f8f413afe22876acb451d73270cddd498
payneLee <peinlcy@sina.cn>
2016-12-20 09:47:34 +0800

    elimination game ac

--- EliminationGame.cpp
+++ EliminationGame.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        
+    	if (n < 4) return (n+2) >> 1;
+    	return (n / 2 & 1) == 0 ? 4 * lastRemaining(n / 4) - 2 : 4 * lastRemaining(n / 4);
     }
 };
